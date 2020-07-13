63aad30fedef1505f471b601c48986282fea0773
Thomas Eblen <tommyeblen@gmail.com>
2020-07-13 05:00:53 -0700

    Updates

---
+++ precache-manifest.1b6c01dca697a619a0557544fce1ebe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cce87b161973d6459d8cb6f297a07ee",
+    "url": "/sudoku-react-app/index.html"
+  },
+  {
+    "revision": "b38ddfc769788dce95bc",
+    "url": "/sudoku-react-app/static/css/main.4e77c76a.chunk.css"
+  },
+  {
+    "revision": "66f5c5b33d52d4d72994",
+    "url": "/sudoku-react-app/static/js/2.2049250f.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/sudoku-react-app/static/js/2.2049250f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38ddfc769788dce95bc",
+    "url": "/sudoku-react-app/static/js/main.41841673.chunk.js"
+  },
+  {
+    "revision": "d981afd160324497f4f3",
+    "url": "/sudoku-react-app/static/js/runtime-main.ea9ed926.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sudoku-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
